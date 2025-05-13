b90305a0249cd029b5eba0eec296e148cbd497b8
Khushbu696 <khushbu.patel.cg@gmail.com>
2025-05-13 15:39:44 +0530

    created footer

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import '../styles/Footer.css'
+
+function Footer() {
+    return (
+        <>
+            <div className="footer">
+                <h3>EchoVault</h3>
+                <div className="footer-items">
+                    <div>HOME</div>
+                    <div>ABOUT US</div>
+                    <div>CONTACT US</div>
+                </div>
+                <p>&copy; 2025 EchoVault. All rights reserved.</p>
+            </div>
+        </>
+    )
+}
+
+export default Footer
