077fbefdcf9535023031f8c93945703af42ac595
Khushbu696 <khushbu.patel.cg@gmail.com>
2025-06-30 11:47:14 +0530

    corrected the paths for about us and contact us page

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -7,8 +7,8 @@ function Footer() {
                 <h3>EchoVault</h3>
                 <div className="footer-items">
                     <div><a href="/" className="footer-link">HOME</a></div>
-                    <div><a href="/about" className="footer-link">ABOUT US</a></div>
-                    <div><a href="/contact" className="footer-link">CONTACT US</a></div>
+                    <div><a href="/about_us" className="footer-link">ABOUT US</a></div>
+                    <div><a href="/contact_us" className="footer-link">CONTACT US</a></div>
                 </div>
                 <p>&copy; 2025 EchoVault. All rights reserved.</p>
             </div>
